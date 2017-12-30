8d624d59ff0c171f186a7179f85e974234245723
IgorDerik <34719600+IgorDerik@users.noreply.github.com>
2017-12-30 19:23:39 +0200

    Create Train.java

---
+++ Lec3/Train.java
@@ -0,0 +1,14 @@
+package Lec3;
+
+public class Train {
+
+    public Train(int id, Station[] stations) {
+        this.id = id;
+        this.stations = stations;
+    }
+
+    private int id;
+    
+    private Station[] stations;
+
+}
