899c35d1c97b8a67f5ae6e865d206459368ce46c
IgorDerik <34719600+IgorDerik@users.noreply.github.com>
2017-12-31 15:24:33 +0200

    Create Station.java

---
+++ Lec3/Station.java
@@ -0,0 +1,22 @@
+package Lec3;
+
+public class Station {
+
+    private String name;
+
+    private int arrivalTime;
+    private int departureTime;
+
+    private String timeFull; //experimental
+
+    public Station(String name, String timeFull) {
+        this.name = name;
+        this.timeFull = timeFull;
+    }
+
+    public Station(String name, int arrivalTime, int departureTime) {
+        this.name = name;
+        this.arrivalTime = arrivalTime;
+        this.departureTime = departureTime;
+    }
+}
