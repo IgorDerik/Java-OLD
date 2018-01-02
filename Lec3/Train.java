d8d49d031214dd6fb8e4b9fe9693d9d6857dc202
IgorDerik <34719600+IgorDerik@users.noreply.github.com>
2018-01-02 13:21:39 +0200

    Update Train.java

--- Lec3/Train.java
+++ Lec3/Train.java
@@ -1,18 +1,102 @@
 package Lec3;
 
+import java.time.LocalTime;
+import java.util.Random;
+
 /*
-TO BE DONE
-*/
+This is not final version
+ */
 
 public class Train {
 
     public Train(int id, Station[] stations) {
         this.id = id;
         this.stations = stations;
+
+        Random random = new Random();
+        availableSeats = 1+random.nextInt(100);
     }
 
     private int id;
 
     private Station[] stations;
 
+    private int availableSeats;
+
+    public int getId() {
+        return id;
+    }
+
+    public int getAvailableSeats() {
+        return availableSeats;
+    }
+
+    public boolean isNearestFromNow (String location) {
+
+        boolean result = false;
+
+        for (Station station : stations) {
+
+            if(location.equals(station.getName())) {
+
+                if(station.getDepartureTime().isAfter(LocalTime.now())) {
+                    result = true;
+
+//                    System.out.print(station.getName()+" ");
+//                    System.out.print(station.getArrivalTime() + " ");
+//                    System.out.println(station.getDepartureTime());
+                }
+
+            }
+
+        }
+
+        return result;
+    }
+
+    public boolean isAfterTime (LocalTime time) {
+//        boolean result = false;
+
+        return stations[0].getDepartureTime().isAfter(time);
+/*
+        for (Station station : stations) {
+            if (station.getDepartureTime().isAfter(time)) {
+                result = true;
+                System.out.print(station.getName()+" ");
+                System.out.println(station.getDepartureTime());
+            }
+        }
+        return result;
+*/
+    }
+
+    public boolean checkDestination (String from, String to) {
+
+//        boolean isFrom=false, isTo=false, result = false;
+        boolean result = false;
+        int indexFrom=-1, indexTo=-1;
+
+        for (int i=0; i<stations.length; i++) {
+
+            if (stations[i].getName().equals(from)) {
+//                isFrom = true;
+                indexFrom = i;
+            }
+
+            if (stations[i].getName().equals(to)) {
+//                isTo = true;
+                indexTo = i;
+            }
+
+        }
+
+//        if(isFrom && isTo) {
+        if(indexFrom>-1 && indexTo>-1) {
+            if (indexFrom<indexTo) {
+                result = true;
+            }
+        }
+
+        return result;
+    }
 }
