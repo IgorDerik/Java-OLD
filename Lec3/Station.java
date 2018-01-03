a2873852eb8dfd219495504dce793405f2c2b1ea
IgorDerik <34719600+IgorDerik@users.noreply.github.com>
2018-01-03 04:44:24 +0200

    Update Station.java

--- Lec3/Station.java
+++ Lec3/Station.java
@@ -13,6 +13,9 @@ public class Station {
     private LocalTime arrivalTime;
     private LocalTime departureTime;
 
+    private LocalTime arrivalTimeBack;
+    private LocalTime departureTimeBack;
+
     public String getName() {
         return name;
     }
@@ -25,10 +28,44 @@ public class Station {
         return arrivalTime;
     }
 
+    public LocalTime getArrivalTimeBack() {
+        return arrivalTimeBack;
+    }
+
+    public LocalTime getDepartureTimeBack() {
+        return departureTimeBack;
+    }
+
+    public void setArrivalTimeBack(LocalTime arrivalTimeBack) {
+        this.arrivalTimeBack = arrivalTimeBack;
+    }
+
+    public void setDepartureTimeBack(LocalTime departureTimeBack) {
+        this.departureTimeBack = departureTimeBack;
+    }
+
     public Station(String name, LocalTime arrivalTime, LocalTime departureTime) {
         this.name = name;
         this.arrivalTime = arrivalTime;
         this.departureTime = departureTime;
     }
 
+    public Station(String name, LocalTime arrivalTime, LocalTime departureTime, LocalTime arrivalTimeBack, LocalTime departureTimeBack) {
+        this.name = name;
+        this.arrivalTime = arrivalTime;
+        this.departureTime = departureTime;
+        this.arrivalTimeBack = arrivalTimeBack;
+        this.departureTimeBack = departureTimeBack;
+    }
+
+    @Override
+    public String toString() {
+        return "\nStation{" +
+                "name='" + name + '\'' +
+                ", arrivalTime=" + arrivalTime +
+                ", departureTime=" + departureTime +
+                ", arrivalTimeBack=" + arrivalTimeBack +
+                ", departureTimeBack=" + departureTimeBack +
+                '}';
+    }
 }
