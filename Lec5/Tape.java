4fc9c3b504e382c1cac9c6f6d1a2f448ea96c404
IgorDerik <34719600+IgorDerik@users.noreply.github.com>
2018-01-17 01:30:20 +0200

    Create Tape.java

---
+++ Lec5/Tape.java
@@ -0,0 +1,17 @@
+package Lec5;
+
+public class Tape {
+
+    public Movie movie() {
+        return _movie;
+    }
+
+    public Tape(String serialNumber, Movie movie) {
+        _serialNumber = serialNumber;
+        _movie = movie;
+    }
+
+    private String _serialNumber;
+    private Movie _movie;
+
+}
