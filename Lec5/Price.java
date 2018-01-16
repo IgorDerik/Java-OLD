8c417b4903f330b63fb11f7aad3bfa25b1cc9384
IgorDerik <34719600+IgorDerik@users.noreply.github.com>
2018-01-17 01:20:16 +0200

    Update Price.java

--- Lec5/Price.java
+++ Lec5/Price.java
@@ -1,1 +1,13 @@
+package Lec5;
 
+public abstract class Price {
+
+//    public abstract int getPriceCode();
+
+    abstract double getCharge(int daysRented);
+
+    public int getFrequentRenterPoints(int daysRented){
+        return 1;
+    }
+
+}
