706f79f17f5ec769deb00f119eac7e3e848ecf81
IgorDerik <34719600+IgorDerik@users.noreply.github.com>
2018-01-22 08:42:48 +0200

    Create IngredientList.java

---
+++ CoffeePoint/products/IngredientList.java
@@ -0,0 +1,11 @@
+package CoffeePoint.products;
+
+public class IngredientList {
+
+    public static Ingredient water = new Ingredient("someWater", 300);
+    public static Ingredient milk = new Ingredient("someMilk",400);
+    public static Ingredient coffee = new Ingredient("someCoffee",500);
+    public static Ingredient cream = new Ingredient("someCream",500);
+    public static Ingredient chocolate = new Ingredient("someChocolate",500);
+
+}
